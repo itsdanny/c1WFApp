c698ada36d2671de38097580ad493c69813aa277
Dan McGregor <dansmcgregor@gmail.com>
2014-01-23 12:54:47 +0000

    this is
    t

---
+++ c1WFApp/Form1.Designer.cs
@@ -0,0 +1,195 @@
+﻿namespace c1WFApp
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.c1Button1 = new C1.Win.C1Input.C1Button();
+            this.c1Ribbon1 = new C1.Win.C1Ribbon.C1Ribbon();
+            this.ribbonApplicationMenu1 = new C1.Win.C1Ribbon.RibbonApplicationMenu();
+            this.ribbonConfigToolBar1 = new C1.Win.C1Ribbon.RibbonConfigToolBar();
+            this.ribbonQat1 = new C1.Win.C1Ribbon.RibbonQat();
+            this.ribbonTab1 = new C1.Win.C1Ribbon.RibbonTab();
+            this.ribbonGroup1 = new C1.Win.C1Ribbon.RibbonGroup();
+            this.c1Button2 = new C1.Win.C1Input.C1Button();
+            this.c1Report1 = new C1.C1Report.C1Report();
+            this.c1Report2 = new C1.C1Report.C1Report();
+            this.c1Report3 = new C1.C1Report.C1Report();
+            this.c1RdlReport1 = new C1.C1Rdl.Rdl2008.C1RdlReport();
+            this.c1RdlReport2 = new C1.C1Rdl.Rdl2008.C1RdlReport();
+            this.c1Button3 = new C1.Win.C1Input.C1Button();
+            ((System.ComponentModel.ISupportInitialize)(this.c1Ribbon1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.c1Report1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.c1Report2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.c1Report3)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // c1Button1
+            // 
+            this.c1Button1.Location = new System.Drawing.Point(68, 97);
+            this.c1Button1.Name = "c1Button1";
+            this.c1Button1.Size = new System.Drawing.Size(75, 23);
+            this.c1Button1.TabIndex = 0;
+            this.c1Button1.Text = "c1Button1";
+            this.c1Button1.UseVisualStyleBackColor = true;
+            // 
+            // c1Ribbon1
+            // 
+            this.c1Ribbon1.ApplicationMenuHolder = this.ribbonApplicationMenu1;
+            this.c1Ribbon1.ConfigToolBarHolder = this.ribbonConfigToolBar1;
+            this.c1Ribbon1.Location = new System.Drawing.Point(0, 0);
+            this.c1Ribbon1.Name = "c1Ribbon1";
+            this.c1Ribbon1.QatHolder = this.ribbonQat1;
+            this.c1Ribbon1.Size = new System.Drawing.Size(754, 156);
+            this.c1Ribbon1.Tabs.Add(this.ribbonTab1);
+            // 
+            // ribbonApplicationMenu1
+            // 
+            this.ribbonApplicationMenu1.Name = "ribbonApplicationMenu1";
+            // 
+            // ribbonConfigToolBar1
+            // 
+            this.ribbonConfigToolBar1.Name = "ribbonConfigToolBar1";
+            // 
+            // ribbonQat1
+            // 
+            this.ribbonQat1.Name = "ribbonQat1";
+            // 
+            // ribbonTab1
+            // 
+            this.ribbonTab1.Groups.Add(this.ribbonGroup1);
+            this.ribbonTab1.Name = "ribbonTab1";
+            this.ribbonTab1.Text = "Tab";
+            // 
+            // ribbonGroup1
+            // 
+            this.ribbonGroup1.Name = "ribbonGroup1";
+            this.ribbonGroup1.Text = "Group";
+            // 
+            // c1Button2
+            // 
+            this.c1Button2.Location = new System.Drawing.Point(112, 68);
+            this.c1Button2.Name = "c1Button2";
+            this.c1Button2.Size = new System.Drawing.Size(75, 23);
+            this.c1Button2.TabIndex = 2;
+            this.c1Button2.Text = "c1Button2";
+            this.c1Button2.UseVisualStyleBackColor = true;
+            // 
+            // c1Report1
+            // 
+            this.c1Report1.ReportDefinition = resources.GetString("c1Report1.ReportDefinition");
+            this.c1Report1.ReportName = "c1Report1";
+            // 
+            // c1Report2
+            // 
+            this.c1Report2.ReportDefinition = resources.GetString("c1Report2.ReportDefinition");
+            this.c1Report2.ReportName = "c1Report2";
+            // 
+            // c1Report3
+            // 
+            this.c1Report3.ReportDefinition = resources.GetString("c1Report3.ReportDefinition");
+            this.c1Report3.ReportName = "c1Report3";
+            // 
+            // c1RdlReport1
+            // 
+            this.c1RdlReport1.Author = null;
+            this.c1RdlReport1.AutoRefresh = 0;
+            this.c1RdlReport1.Code = null;
+            this.c1RdlReport1.ConsumeContainerWhitespace = false;
+            this.c1RdlReport1.DataElementName = null;
+            this.c1RdlReport1.DataElementStyle = C1.C1Rdl.Rdl2008.DataElementStyle.Attribute;
+            this.c1RdlReport1.DataSchema = null;
+            this.c1RdlReport1.DataTransform = null;
+            this.c1RdlReport1.DeferVariableEvaluation = false;
+            this.c1RdlReport1.Description = null;
+            this.c1RdlReport1.RdlVersion = C1.C1Rdl.RdlFormatVersion.RDL2008;
+            this.c1RdlReport1.ReportDefinition = resources.GetString("c1RdlReport1.ReportDefinition");
+            // 
+            // c1RdlReport2
+            // 
+            this.c1RdlReport2.Author = null;
+            this.c1RdlReport2.AutoRefresh = 0;
+            this.c1RdlReport2.Code = null;
+            this.c1RdlReport2.ConsumeContainerWhitespace = false;
+            this.c1RdlReport2.DataElementName = null;
+            this.c1RdlReport2.DataElementStyle = C1.C1Rdl.Rdl2008.DataElementStyle.Attribute;
+            this.c1RdlReport2.DataSchema = null;
+            this.c1RdlReport2.DataTransform = null;
+            this.c1RdlReport2.DeferVariableEvaluation = false;
+            this.c1RdlReport2.Description = null;
+            this.c1RdlReport2.RdlVersion = C1.C1Rdl.RdlFormatVersion.RDL2008;
+            this.c1RdlReport2.ReportDefinition = resources.GetString("c1RdlReport2.ReportDefinition");
+            // 
+            // c1Button3
+            // 
+            this.c1Button3.Location = new System.Drawing.Point(255, 109);
+            this.c1Button3.Name = "c1Button3";
+            this.c1Button3.Size = new System.Drawing.Size(75, 23);
+            this.c1Button3.TabIndex = 4;
+            this.c1Button3.Text = "c1Button3";
+            this.c1Button3.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(754, 459);
+            this.Controls.Add(this.c1Button3);
+            this.Controls.Add(this.c1Button2);
+            this.Controls.Add(this.c1Ribbon1);
+            this.Controls.Add(this.c1Button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.c1Ribbon1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.c1Report1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.c1Report2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.c1Report3)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private C1.Win.C1Input.C1Button c1Button1;
+        private C1.Win.C1Ribbon.C1Ribbon c1Ribbon1;
+        private C1.Win.C1Ribbon.RibbonApplicationMenu ribbonApplicationMenu1;
+        private C1.Win.C1Ribbon.RibbonConfigToolBar ribbonConfigToolBar1;
+        private C1.Win.C1Ribbon.RibbonQat ribbonQat1;
+        private C1.Win.C1Ribbon.RibbonTab ribbonTab1;
+        private C1.Win.C1Ribbon.RibbonGroup ribbonGroup1;
+        private C1.Win.C1Input.C1Button c1Button2;
+        private C1.C1Report.C1Report c1Report1;
+        private C1.C1Report.C1Report c1Report2;
+        private C1.C1Report.C1Report c1Report3;
+        private C1.C1Rdl.Rdl2008.C1RdlReport c1RdlReport1;
+        private C1.C1Rdl.Rdl2008.C1RdlReport c1RdlReport2;
+        private C1.Win.C1Input.C1Button c1Button3;
+    }
+}
+
